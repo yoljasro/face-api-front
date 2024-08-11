0e22b005f4d3a7cdc49cbaff6c0f72859176bd4e
yoljasro <saidaliyevjasur450@gmail.com>
2024-08-11 14:15:13 +0500

    changes

--- src/pages/api/users.ts
+++ src/pages/api/users.ts
@@ -3,6 +3,8 @@ import { NextApiRequest, NextApiResponse } from 'next';
 const users = [
   { name  : "Behzod " , imageUrl : "/uploads/behzod.jpg"},
   { name  : "Jasur " , imageUrl : "/uploads/jasur.jpg"},
+  { name  : "Aziz Manapov " , imageUrl : "/uploads/Aziz.jpg"},
+  { name  : "Shuhratjon SaitMuradov " , imageUrl : "/uploads/shuhratjon.jpg"},
   // Yana foydalanuvchilar qo'shishingiz mumkin
 ];
 
