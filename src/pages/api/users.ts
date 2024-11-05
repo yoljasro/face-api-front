8199798912a323146a25d4b0ed8e83f72254f6e1
yoljasro <saidaliyevjasur450@gmail.com>
2024-11-05 13:06:55 +0500

    change url , added users

--- src/pages/api/users.ts
+++ src/pages/api/users.ts
@@ -1,11 +1,18 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 
 const users = [
-  { name  : "Behzod " , imageUrl : "/uploads/behzod.jpg"},
-  { name  : "Jasur " , imageUrl : "/uploads/jasur.jpg"},
-  { name  : "Aziz Manapov " , imageUrl : "/uploads/Aziz.jpg"},
-  { name  : "Shuhratjon SaitMuradov " , imageUrl : "/uploads/shuhratjon.jpg"},
-  // Yana foydalanuvchilar qo'shishingiz mumkin
+  { name: "Behzod ", imageUrl: "/uploads/behzod.jpg" },
+  { name: "Jasur ", imageUrl: "/uploads/jasur.jpg" },
+  { name: "Aziz Manapov ", imageUrl: "/uploads/Aziz.jpg" },
+  { name: "Shuhratjon SaitMuradov ", imageUrl: "/uploads/shuhratjon.jpg" },
+  { name: "Shahnoza ", imageUrl: "/uploads/one.jpg" },
+  { name: "Yusuf ", imageUrl: "/uploads/two.jpg" },
+  { name: " Shuhrat ", imageUrl: "/uploads/three.jpg" },
+  { name: " Elbek ", imageUrl: "/uploads/four.jpg" },
+  { name: " Murod ", imageUrl: "/uploads/five.jpg" },
+  { name: " Svetlana ", imageUrl: "/uploads/six.jpg" },
+                                                                                                                                                                                                                        
+
 ];
 
 export default (req: NextApiRequest, res: NextApiResponse) => {
