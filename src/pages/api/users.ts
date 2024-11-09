0038cdfb561bab2c356e3e650b9c3a16f83ae8bb
yoljasro <saidaliyevjasur450@gmail.com>
2024-11-09 17:24:34 +0500

    change users

--- src/pages/api/users.ts
+++ src/pages/api/users.ts
@@ -2,15 +2,17 @@ import { NextApiRequest, NextApiResponse } from 'next';
 
 const users = [
   { name: "Behzod ", imageUrl: "/uploads/behzod.jpg" },
-  { name: "Jasur ", imageUrl: "/uploads/jasur.jpg" },
+  { name: "Jasur", imageUrl: "/uploads/jasur.jpg" },
   { name: "Aziz Manapov ", imageUrl: "/uploads/Aziz.jpg" },
   { name: "Shuhratjon SaitMuradov ", imageUrl: "/uploads/shuhratjon.jpg" },
   { name: "Shahnoza ", imageUrl: "/uploads/one.jpg" },
   { name: "Yusuf ", imageUrl: "/uploads/two.jpg" },
-  { name: " Shuhrat ", imageUrl: "/uploads/three.jpg" },
-  { name: " Elbek ", imageUrl: "/uploads/four.jpg" },
-  { name: " Murod ", imageUrl: "/uploads/five.jpg" },
-  { name: " Svetlana ", imageUrl: "/uploads/six.jpg" },
+  { name: "Shuhrat ", imageUrl: "/uploads/three.jpg" },
+  { name: "Elbek ", imageUrl: "/uploads/four.jpg" },
+  { name: "Anvar ", imageUrl: "/uploads/anvar.jpg" },
+  { name: "Olya", imageUrl: "/uploads/olya.jpg" },
+  { name: "Bobur ", imageUrl: "/uploads/five.jpg" },
+  { name: "Svetlana ", imageUrl: "/uploads/six.jpg" },
                                                                                                                                                                                                                         
 
 ];
